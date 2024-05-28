2dfb9aeb068c7e27bcdd1fc85ab373dff7d49bbe
Raymond <rtperez1020@gmail.com>
2024-05-27 23:23:54 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -19,12 +20,34 @@ function Header(){
   </header>)
 }
 
+function CoreConcept(props){
+  return (
+  <li>
+    <img src={props.image} alt={props.title}/>
+    <h3>{props.title}</h3>
+    <p>{props.description}</p>
+  </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+              title="Components" 
+              description="The core UI building block."
+              image={componentsImg}/>
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+
+          </ul>
+        </section>
       </main>
     </div>
   );
