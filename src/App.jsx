1a7df29228c62bd7ecf4a7f5f068e5757131c44a
Raymond <rtperez1020@gmail.com>
2024-06-17 00:05:32 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { CORE_CONCEPTS } from './data.js'
+import { CORE_CONCEPTS } from './data.js' 
 import Header from './components/Header.jsx'
 import CoreConcept from './components/CoreConcept.jsx'
 
